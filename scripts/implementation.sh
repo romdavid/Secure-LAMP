f7ceb91ae12d67d10187062233f5d33effaba784
David <davidnickromero@gmail.com>
2021-04-17 01:21:01 -0700

    Added Scripts

--- scripts/implementation.sh
+++ scripts/implementation.sh
@@ -5,9 +5,10 @@ sudo apt-get update
 
 #IP Tables
 sudo apt install iptables-persistent
-sudo iptables -t filter -P INPUT DROP
-sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 sudo iptables-save > /etc/iptables/rules.v4
