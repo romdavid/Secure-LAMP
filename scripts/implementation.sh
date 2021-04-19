4ba17345bcb488e8ec733f096f082d0f7ddc786f
David <davidnickromero@gmail.com>
2021-04-18 21:18:36 -0700

    Fixed firewall script

--- scripts/implementation.sh
+++ scripts/implementation.sh
@@ -5,10 +5,11 @@ sudo apt-get update
 
 #IP Tables
 sudo apt install iptables-persistent
-sudo iptables -P INPUT DROP
 sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
-sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -P INPUT DROP
 sudo iptables-save > /etc/iptables/rules.v4
