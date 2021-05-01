b2d43efe64fbb96edc32ec48bed63b28552fbd29
David <davidnickromero@gmail.com>
2021-05-01 16:54:38 -0700

    Sanitizing input and bug fix

--- backend/db.php
+++ backend/db.php
@@ -8,12 +8,14 @@
 			$dbname = $config['dbname'];
 			$this->connect = mysqli_connect($host, $username, $password, $dbname) or die ("cannot connect");
 		}
-		
+
 		function escape($input) {
 			return mysqli_real_escape_string($this->connect, $input);
 		}
-		
+
 		function verify_user_email($user, $email) {
+			$user = $this->escape($user);
+			$email = $this->escape($email);
 			$result = mysqli_query($this->connect, "SELECT id FROM login WHERE username='$user'");
                         if (mysqli_num_rows($result) == 1) {
                                 $user_id = mysqli_fetch_assoc($result)[id];
@@ -29,6 +31,8 @@
 		}
 
 		function get_active_id($username, $password) {
+			$username = $this->escape($username);
+			$password = $this->escape($password);
 			$result = mysqli_query($this->connect, "SELECT id FROM login WHERE password='$password' AND username='$username' AND activated=1");
 			if ($result) {
 				$id = mysqli_fetch_assoc($result)['id'];
@@ -39,6 +43,7 @@
 		}
 
 		function create_mfa($id) {
+			$id = $this->escape($id);
 			$code = rand(0,9).rand(0,9).rand(0,9).rand(0,9).rand(0,9);
 			if (mysqli_query($this->connect, "UPDATE login SET mfa='$code' WHERE id='$id'")) {
         			return $code;
@@ -48,6 +53,7 @@
 		}
 
 		function verify_mfa($code) {
+			$code = $this->escape($code);
 			$result = mysqli_query($this->connect, "SELECT id FROM login WHERE mfa='$code'");
 			if (mysqli_num_rows($result) == 1) {
 			        return mysqli_fetch_assoc($result)['id'];
@@ -57,10 +63,12 @@
 		}
 
 		function destroy_mfa($code) {
+			$code = $this->escape($code);
 			return mysqli_query($this->connect, "UPDATE login SET mfa=NULL WHERE mfa='$code'");
 		}
 
 		function get_info($id) {
+			$id = $this->escape($id);
 			$result = mysqli_query($this->connect, "SELECT first,last,logins,DATE_FORMAT(prev_login,'%b %d, %Y') AS formatted_date FROM info WHERE id='$id'");
 			if ($result) {
 				$data = mysqli_fetch_assoc($result);
@@ -71,14 +79,17 @@
 		}
 		
 		function update_info($id) {
+			$id = $this->escape($id);
 			return mysqli_query($this->connect, "UPDATE info SET logins=logins+1,prev_login=CURDATE() where id='$id'");
 		}
 		
 		function destroy_token($token) {
+			$token = $this->escape($token);
 			return mysqli_query($this->connect, "UPDATE login SET token=NULL WHERE token='$token'");
 		}
 		
 		function get_usertoken($email) {
+			$email = $this->escape($email);
 			$result = mysqli_query($this->connect, "SELECT username FROM login WHERE email='$email'");
 			if ($result) {
 				$username = mysqli_fetch_assoc($result)['username'];
@@ -91,6 +102,7 @@
 		}
 		
 		function verify_token($token) {
+			$token = $this->escape($token);
 			$result = mysqli_query($this->connect, "SELECT id FROM login WHERE token='$token'");
 			if ($result) {
 				$id = mysqli_fetch_assoc($result)['id'];
@@ -101,6 +113,8 @@
 		}
 		
 		function verify_token_user($token, $username) {
+			$token = $this->escape($token);
+			$username = $this->escape($username);
 			$result = mysqli_query($this->connect, "SELECT username FROM login WHERE token='$token'");
 			if ($result) {
 				$user = mysqli_fetch_assoc($result)['username'];
@@ -111,6 +125,9 @@
 		}
 		
 		function verify_answers($id, $ans1, $ans2) {
+			$id = $this->escape($id);
+			$ans1 = $this->escape($ans1);
+			$ans2 = $this->escape($ans2);
 			$result = mysqli_query($this->connect, "SELECT answer1,answer2 FROM info WHERE id='$id'");
 			if ($result) {
 				$answers = mysqli_fetch_assoc($result);
@@ -122,6 +139,7 @@
 		}
 		
 		function get_questions($mail) {
+			$mail = $this->escape($mail);
 			$result = mysqli_query($this->connect, "SELECT id FROM login WHERE email='$mail'");
 			if (mysqli_num_rows($result) == 1) {
 				$id = mysqli_fetch_assoc($result)['id'];
@@ -135,6 +153,7 @@
 		}
 		
 		function get_email($id) {
+			$id = $this->escape($id);
 			$result = mysqli_query($this->connect, "SELECT email FROM login WHERE id='$id'");
 			if ($result) {
 				$email = mysqli_fetch_assoc($result)['email'];
@@ -145,14 +164,27 @@
 		}
 		
 		function reset_password($user, $newpassword) {
+			$user = $this->escape($user);
+			$newpassword = $this->escape($newpassword);
 			return mysqli_query($this->connect, "UPDATE login SET password='$newpassword' WHERE username='$user'");
 		}
 		
 		function activate($id) {
+			$id = $this->escape($id);
 			return mysqli_query($this->connect, "UPDATE login SET activated=1 WHERE id='$id'");
 		}
 		
 		function create_user($user,$pass,$token,$email,$first,$last,$birth,$q1,$q2,$ans1,$ans2) {
+			$user = $this->escape($user);
+			$pass = $this->escape($pass);
+			$token = $this->escape($token);
+			$email = $this->escape($email);
+			$first = $this->escape($first);
+			$last = $this->escape($last);
+			$q1 = $this->escape($q1);
+			$q2 = $this->escape($q2);
+			$ans1 = $this->escape($ans1);
+			$ans2 = $this->escape($ans2);
 			$query = "INSERT INTO login VALUES(NULL,'$user','$pass',0,'$token','$email',NULL);";
 			$query .= "INSERT INTO info VALUES(NULL,'$first','$last','$birth',0,NULL,'$q1','$q2','$ans1','$ans2')";
 			if (mysqli_multi_query($this->connect, $query)) {
